b2224ec442e96a6ab12c580bd0f913121bea0566
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2022-12-28 19:27:26 +0000

    Update init_vm_run_as_root.sh

--- init_vm_run_as_root.sh
+++ init_vm_run_as_root.sh
@@ -4,7 +4,7 @@ touch ~/.hushlogin
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
 printf "LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8" > /etc/default/locale
 apt update
-useradd -d /home/blackops -m -G sudo -s /bin/bash -p "\$6\$foi8G0Lu2xTnf3ac\$z4maNK3VG.lkR0IRx4kaDnQOs4J91pqkF39XFrWTgcWybwGF/8erGV4sktOiNy/AuG4OZ1vv.ftEXX.NxrcHi/" blackops
+useradd -d /home/blackops -m -G sudo -s /bin/bash -p "\$6\$MUqx/xpb7L4a1Yl2\$ClfzkoicrRyJsAiXRbLuG8NonKQI7cUgpwOhQr7QJnwt2mZkmQHDGisoBHiXfqzEM3DFscpHc6hDtMak1ff3H/" blackops
 sudo -u blackops touch /home/blackops/.hushlogin
 apt -y install unzip
 apt -y install python3-pip
