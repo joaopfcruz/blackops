dfbc6ffb17a7180bd5a4d08654a76abb6d1278e3
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2022-12-28 01:37:42 +0000

    Add censys

---
+++ etc/censys/run.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#This script runs censys tool
+#arguments <WORKSPACE> <DOMAIN>
+
+usage() { echo "Usage: $0 -w <workspace> -d <domain>" 1>&2; exit 0; }
+
+while getopts ":w:d:" flags; do
+	case "${flags}" in
+		w)
+			workspace=${OPTARG}
+			;;
+		d)
+			domain=${OPTARG}
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+BLACKOPS_DATA_FOLDER="${BLACKOPS_HOMEDIR}/data"
+OUTPUT_ORG_FOLDER="${BLACKOPS_DATA_FOLDER}/${workspace}"
+OUTPUT_RECON_FOLDER="${OUTPUT_ORG_FOLDER}/reconnaissance"
+OUTPUT_SEARCH_FILENAME="censys.search.out.${domain}.$(date +'%Y_%m_%dT%H_%M_%S').json"
+OUTPUT_SUBDOMAINS_FILENAME="censys.subdomains.out.${domain}.$(date +'%Y_%m_%dT%H_%M_%S').json"
+
+if [ -z "${workspace}" ] || [ -z "${domain}" ]; then
+	usage
+fi
+
+if [ -z "${BLACKOPS_HOMEDIR}" ]; then
+	echo "envvar \"BLACKOPS_HOMEDIR\" is not set. Did you setup the environment with makefile?"
+	exit 1
+fi
+
+if ! [ -d "${BLACKOPS_DATA_FOLDER}" ]; then
+	echo "\"${BLACKOPS_DATA_FOLDER}\" does not exist. Did you setup the environment with makefile?"
+        exit 1
+fi
+
+if ! [ -d "${OUTPUT_ORG_FOLDER}" ]; then
+	echo "\"${OUTPUT_ORG_FOLDER}\" does not exist. Creating."
+	if ! [ -w "${BLACKOPS_DATA_FOLDER}" ]; then
+		echo "Can't create \"${OUTPUT_ORG_FOLDER}\". Missing write permission.  Exiting."
+		exit 1
+	else
+		mkdir "${OUTPUT_ORG_FOLDER}"
+	fi
+fi
+
+if ! [ -d "${OUTPUT_RECON_FOLDER}" ]; then
+        echo "\"${OUTPUT_RECON_FOLDER}\" does not exist. Creating."
+        if ! [ -w "${OUTPUT_ORG_FOLDER}" ]; then
+                echo "Can't create \"${OUTPUT_RECON_FOLDER}\". Missing write permission.  Exiting."
+                exit 1
+        else
+                mkdir "${OUTPUT_RECON_FOLDER}"
+        fi
+fi
+
+
+#run:
+censys search "${domain}" > "${OUTPUT_RECON_FOLDER}/${OUTPUT_SEARCH_FILENAME}"
+#compress output (saving space ftw!)
+bzip2 -z "${OUTPUT_RECON_FOLDER}/${OUTPUT_SEARCH_FILENAME}"
+
+censys subdomains "${domain}" > "${OUTPUT_RECON_FOLDER}/${OUTPUT_SUBDOMAINS_FILENAME}"
+#compress output (saving space ftw!)
+bzip2 -z "${OUTPUT_RECON_FOLDER}/${OUTPUT_SUBDOMAINS_FILENAME}"
