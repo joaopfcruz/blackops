684fd30a066a3d403c2a563925ed92f0a9a37e4e
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2022-12-28 16:48:23 +0000

    Update init_vm_run_as_root.sh

--- init_vm_run_as_root.sh
+++ init_vm_run_as_root.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 touch ~/.hushlogin
-echo "127.0.0.1\t$(hostname)" >> /etc/hosts
+echo "127.0.0.1 $(hostname)" >> /etc/hosts
 printf "LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8" > /etc/default/locale
 apt update
 useradd -d /home/blackops -m -G sudo -s /bin/bash -p "\$6\$foi8G0Lu2xTnf3ac\$z4maNK3VG.lkR0IRx4kaDnQOs4J91pqkF39XFrWTgcWybwGF/8erGV4sktOiNy/AuG4OZ1vv.ftEXX.NxrcHi/" blackops
