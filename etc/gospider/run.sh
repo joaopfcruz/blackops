40069c6cfd10d945fc3d23c5af64b63af5e4692b
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2022-12-29 19:07:30 +0000

    Update run.sh

--- etc/gospider/run.sh
+++ etc/gospider/run.sh
@@ -1,51 +1,53 @@
 #!/bin/bash
 #This script runs gospider tool
-#arguments <WORKSPACE> <DOMAIN>
+#arguments <WORKSPACE> <URL>
 
-usage() { echo "Usage: $0 -w <workspace> -d <domain>" 1>&2; exit 0; }
+usage() { echo "Usage: $0 -w <workspace> -u <url>" 1>&2; exit 0; }
 
-while getopts ":w:d:" flags; do
-        case "${flags}" in
-                w)
-                        workspace=${OPTARG}
-                        ;;
-                d)
-                        domain=${OPTARG}
-                        ;;
-                *)
-                        usage
-                        ;;
-        esac
+while getopts ":w:u:" flags; do
+	case "${flags}" in
+		w)
+			workspace=${OPTARG}
+			;;
+		u)
+			url=${OPTARG}
+			;;
+		*)
+			usage
+			;;
+	esac
 done
 shift $((OPTIND-1))
+
+sanitized_url=$(sed -e "s/[^A-Za-z0-9._-]/_/g" <<< "$url")
 
 BLACKOPS_DATA_FOLDER="${BLACKOPS_HOMEDIR}/data"
 OUTPUT_ORG_FOLDER="${BLACKOPS_DATA_FOLDER}/${workspace}"
 OUTPUT_SCANNING_FOLDER="${OUTPUT_ORG_FOLDER}/scanning"
-OUTPUT_FILENAME="gospider.out.${domain}.$(date +'%Y_%m_%dT%H_%M_%S').json"
+OUTPUT_FILENAME="gospider.out.${sanitized_url}.$(date +'%Y_%m_%dT%H_%M_%S').json"
 
-if [ -z "${workspace}" ] || [ -z "${domain}" ]; then
-        usage
+if [ -z "${workspace}" ] || [ -z "${url}" ]; then
+	usage
 fi
 
 if [ -z "${BLACKOPS_HOMEDIR}" ]; then
-        echo "envvar \"BLACKOPS_HOMEDIR\" is not set. Did you setup the environment with makefile?"
-        exit 1
+	echo "envvar \"BLACKOPS_HOMEDIR\" is not set. Did you setup the environment with makefile?"
+	exit 1
 fi
 
 if ! [ -d "${BLACKOPS_DATA_FOLDER}" ]; then
-        echo "\"${BLACKOPS_DATA_FOLDER}\" does not exist. Did you setup the environment with makefile?"
+	echo "\"${BLACKOPS_DATA_FOLDER}\" does not exist. Did you setup the environment with makefile?"
         exit 1
 fi
 
 if ! [ -d "${OUTPUT_ORG_FOLDER}" ]; then
-        echo "\"${OUTPUT_ORG_FOLDER}\" does not exist. Creating."
-        if ! [ -w "${BLACKOPS_DATA_FOLDER}" ]; then
-                echo "Can't create \"${OUTPUT_ORG_FOLDER}\". Missing write permission.  Exiting."
-                exit 1
-        else
-                mkdir "${OUTPUT_ORG_FOLDER}"
-        fi
+	echo "\"${OUTPUT_ORG_FOLDER}\" does not exist. Creating."
+	if ! [ -w "${BLACKOPS_DATA_FOLDER}" ]; then
+		echo "Can't create \"${OUTPUT_ORG_FOLDER}\". Missing write permission.  Exiting."
+		exit 1
+	else
+		mkdir "${OUTPUT_ORG_FOLDER}"
+	fi
 fi
 
 if ! [ -d "${OUTPUT_SCANNING_FOLDER}" ]; then
@@ -59,6 +61,6 @@ if ! [ -d "${OUTPUT_SCANNING_FOLDER}" ]; then
 fi
 
 #run:
-gospider -s "{domain}" -u web -t 10 --subs --other-source --include-subs --sitemap --robots -q --json > "${OUTPUT_SCANNING_FOLDER}/${OUTPUT_FILENAME}"
+gospider -s ${url} -u web -t 10 --subs --other-source --include-subs --sitemap --robots -q --json > "${OUTPUT_SCANNING_FOLDER}/${OUTPUT_FILENAME}"
 #compress output (saving space ftw!)
 bzip2 -z "${OUTPUT_SCANNING_FOLDER}/${OUTPUT_FILENAME}"
