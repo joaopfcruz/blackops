cf57f1682ffb4892f6440571ea1bbe9af260d254
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2022-12-29 16:08:03 +0000

    install go and python3

--- init_vm_run_as_root.sh
+++ init_vm_run_as_root.sh
@@ -7,9 +7,11 @@ apt update
 useradd -d /home/blackops -m -G sudo -s /bin/bash -p "\$6\$MUqx/xpb7L4a1Yl2\$ClfzkoicrRyJsAiXRbLuG8NonKQI7cUgpwOhQr7QJnwt2mZkmQHDGisoBHiXfqzEM3DFscpHc6hDtMak1ff3H/" blackops
 sudo -u blackops touch /home/blackops/.hushlogin
 apt -y install unzip
+apt -y install python3
 apt -y install python3-pip
 apt -y install bzip2
 apt -y install make
+apt -y install golang-go
 rm -rf /home/blackops/blackops_repo
 git clone https://github.com/joaopfcruz/blackops.git /home/blackops/blackops_repo
 chown -R blackops:blackops /home/blackops/blackops_repo
