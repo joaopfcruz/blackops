645c76179e344b35edaf1bd25ed1d6afc7f7d0fb
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-01-02 20:15:17 +0000

    Removed metasploit

--- init_vm_run_as_root.sh
+++ init_vm_run_as_root.sh
@@ -14,7 +14,6 @@ apt -y install bzip2
 apt -y install make
 apt -y install golang-go
 apt -y install ruby
-apt -y install metasploit-framework
 rm -rf /home/blackops/blackops_repo
 git clone https://github.com/joaopfcruz/blackops.git /home/blackops/blackops_repo
 chown -R blackops:blackops /home/blackops/blackops_repo
